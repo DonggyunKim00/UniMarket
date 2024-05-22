b0523366160a8768b3a89a8bb589a8cdde71d297
ppacman <smwfkim2@naver.com>
2024-05-22 16:23:11 +0900

    feat. 메인 페이지 퍼블리싱

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
-import Header from '../components/common/Header';
+import styled from 'styled-components';
+import PreviewList from '../components/common/PreviewList';
+import PageWrapper from '../components/common/PageWrapper';
 
 const Home = () => {
   return (
-    <div>
-      <Header />
-    </div>
+    <PageWrapper>
+      <Wrapper>
+        <PreviewList
+          sellState={1}
+          startPrice={1000}
+          nowPrice={10000}
+          endDate={new Date('2024-05-13')}
+        />
+        <PreviewList
+          sellState={0}
+          startPrice={2000}
+          nowPrice={20000}
+          endDate={new Date('2024-05-13')}
+        />
+      </Wrapper>
+    </PageWrapper>
   );
 };
 
 export default Home;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  gap: 20px;
+  height: 900px;
+`;
