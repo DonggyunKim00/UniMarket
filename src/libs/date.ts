4fd6e124c749135c2b3a33cf9d1bb8205ec585bc
donggyun <donggyunkim12@gmail.com>
2024-05-30 17:19:19 +0900

    feat: 날짜 포맷터 함수 구현

--- src/libs/date.ts
+++ src/libs/date.ts
@@ -7,3 +7,18 @@ export const parseDate = (date: string) => {
   const parseDate = Date.parse(date);
   return { parseDate };
 };
+
+export const formattingDate = (str: string) => {
+  const date = new Date(str);
+
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+
+  const formattedDate = `${year}-${month}-${day} / ${hours}:${minutes}:${seconds}`;
+
+  return formattedDate;
+};
