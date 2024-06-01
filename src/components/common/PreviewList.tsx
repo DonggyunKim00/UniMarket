6518689aefec4ef0c3f0521aca84ffe374c8f58f
ppacman <smwfkim2@naver.com>
2024-06-01 10:17:17 +0900

    feat. 데이터 렌더링 및 상품 클릭시 해당 상품 페이지로 라우팅

--- src/components/common/PreviewList.tsx
+++ src/components/common/PreviewList.tsx
@@ -1,46 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import divideNum from '../../libs/divideNum';
 import { useNavigate } from 'react-router-dom';
 
-interface ItemType {
-  sellState: number;
-  startPrice: number;
-  nowPrice: number;
-  endDate: Date;
-}
-
-const PreviewList = ({
-  sellState,
-  startPrice,
-  nowPrice,
-  endDate,
-}: ItemType) => {
+const PreviewList = ({ data, state }: any) => {
   const navigate = useNavigate();
+
+  const itemClick = (id: number) => {
+    navigate(`/detail?id=${id}`);
+  };
 
   return (
     <Wrapper>
       <MenuLine>
-        <SellState>{sellState ? '경매중' : '경매전'}</SellState>
+        <SellState>{state ? '경매전' : '경매중'}</SellState>
         <DetailBtn onClick={() => navigate('/list')}>
           더보기
           <ArrowForwardIosIcon />
         </DetailBtn>
       </MenuLine>
       <ListBox>
-        <ItemBox>
-          <img src="./first.png" alt="" width={60} height={60} />
-          <div>현재 입찰가</div>
-          <div>경매 시작가</div>
-          <div>낙찰 예정시간</div>
-        </ItemBox>
-        <ItemBox>
-          <img src="./first.png" alt="" width={60} height={60} />
-        </ItemBox>
-        <ItemBox>
-          <img src="./first.png" alt="" width={60} height={60} />
-        </ItemBox>
+        {data?.auction.map((item: any, id: number) => (
+          <ItemBox
+            key={item.prouduct_id}
+            onClick={() => itemClick(item.product_id)}
+          >
+            <img
+              src={item.image || './first.png'}
+              alt=""
+              width={60}
+              height={60}
+            />
+            <DetailBox>
+              <ItemDetail> 제목 : {item.title}</ItemDetail>
+              <ItemDetail>설명 : {item.describe}</ItemDetail>
+
+              {state ? null : (
+                <ItemDetail>현재 입찰가: {item.current_bid}</ItemDetail>
+              )}
+              <ItemDetail>경매 시작가: {item.min_price}</ItemDetail>
+              <ItemDetail>낙찰 예정시간: {item.end_date}</ItemDetail>
+            </DetailBox>
+          </ItemBox>
+        ))}
       </ListBox>
     </Wrapper>
   );
@@ -51,10 +53,10 @@ export default PreviewList;
 const Wrapper = styled.div`
   display: flex;
   width: 350px;
-  height: 350px;
   justify-content: space-between;
   flex-direction: column;
   gap: 30px;
+  margin-top: 20px;
 `;
 
 const MenuLine = styled.div`
@@ -80,8 +82,8 @@ const ListBox = styled.div`
   align-items: center;
   padding: 20px;
   flex-direction: column;
-  justify-content: space-between;
-  gap: 20px;
+  gap: 30px;
+  min-height: 350px;
 `;
 const ItemBox = styled.div`
   width: 90%;
@@ -89,9 +91,16 @@ const ItemBox = styled.div`
   border-radius: 20px;
   padding: 10px;
   display: flex;
+  min-height: 120px;
+  align-items: center;
+  cursor: pointer;
 `;
-const Sell = styled.div``;
-
-const Now = styled.div``;
-const End = styled.div``;
-const ItemImage = styled.div``;
+const ItemDetail = styled.div`
+  overflow: hidden;
+  overflow-y: hidden;
+  height: 20px;
+`;
+const DetailBox = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
