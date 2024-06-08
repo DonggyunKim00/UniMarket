f7963c7600b5edba622f46fa1cd032a92b8c9a40
ppacman <smwfkim2@naver.com>
2024-06-08 13:23:03 +0900

    feat. detail 페이지 api 연동

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -2,33 +2,63 @@ import React from 'react';
 import styled from 'styled-components';
 import UndoIcon from '@mui/icons-material/Undo';
 import PageWrapper from '../components/common/PageWrapper';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useGetDetail } from '../hooks/query/useGetDetail';
+import LoadingSpinner from '../components/common/LoadingSpinner';
 const Detail = () => {
+  const [searchParams] = useSearchParams();
+  const idString = searchParams.get('id');
+  const id = idString ? parseInt(idString) : null;
+
+  const { data, isLoading } = useGetDetail(id as number);
+
+  const navigate = useNavigate();
+
+  const undoClick = () => {
+    navigate(-1);
+  };
+
+  const item = data?.product[0];
   return (
     <PageWrapper>
-      <Wrapper>
-        <Header>
-          <UndoIcon sx={{ fontSize: 30 }} />
-          <h2>상품 제목</h2>
-        </Header>
-        <ItemWrapper>
-          <ItemImage>
-            <img src={'./first.png'} width={385} height={240} alt="" />
-          </ItemImage>
-          <StartPrice>
-            시작 입찰가 <br />
-            100만원
-          </StartPrice>
-          <NowPrice>
-            현재 입찰가 <br />
-            3000만원
-          </NowPrice>
-          <ItemInfo>설명을 적을겁니다 자세하게</ItemInfo>
-          <Chart>
-            <img src="./chart.png" width={385} height={240} alt="" />
-          </Chart>
-          <BuyButton>구매입찰</BuyButton>
-        </ItemWrapper>
-      </Wrapper>
+      {isLoading ? (
+        <LoadingSpinner width={50} height={50} $borderWidth={4} />
+      ) : (
+        <Wrapper>
+          <Header>
+            <div
+              style={{
+                position: 'absolute',
+                left: 0,
+              }}
+            >
+              <UndoIcon
+                sx={{ cursor: 'pointer', fontSize: 30 }}
+                onClick={() => undoClick()}
+              />
+            </div>
+            <ItemTitle>{item?.title}</ItemTitle>
+          </Header>
+          <ItemWrapper>
+            <ItemImage>
+              <img src={'./first.png'} width={385} height={240} alt="" />
+            </ItemImage>
+            <StartPrice>
+              시작 입찰가 <br />
+              {item?.min_price}
+            </StartPrice>
+            <NowPrice>
+              현재 입찰가 <br />
+              {item?.current_bid}
+            </NowPrice>
+            <ItemInfo>{item?.describe}</ItemInfo>
+            <Chart>
+              <img src="./chart.png" width={385} height={240} alt="" />
+            </Chart>
+            <BuyButton>구매입찰</BuyButton>
+          </ItemWrapper>
+        </Wrapper>
+      )}
     </PageWrapper>
   );
 };
@@ -44,9 +74,11 @@ const Wrapper = styled.div`
 const Header = styled.div`
   display: flex;
   align-items: center;
-  gap: 120px;
+  justify-content: center;
+  position: relative;
 `;
 
+const ItemTitle = styled.h2``;
 const ItemWrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -88,5 +120,3 @@ const BuyButton = styled.div`
   font-weight: 600;
   border-radius: 40px;
 `;
-
-const ButtonBox = styled.div``;
