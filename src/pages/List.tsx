32c508d7c0c32bb25d9ca6c38253675a4226616c
donggyun <donggyunkim12@gmail.com>
2024-06-11 22:04:56 +0900

    feat: list 페이지 경매 전,후 데이터베이스 연동 #27

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { styled } from 'styled-components';
+import PageWrapper from '../components/common/PageWrapper';
+import ProductCard from '../components/common/ProductCard';
+import ToggleButton from '../components/list/ToggleButton';
+import {
+  useGetAfterAuctionList,
+  useGetBeforeAuctionList,
+} from '../hooks/query/useProduct';
+import { getNow } from '../libs/date';
 
 const List = () => {
-  return <div>상품 리스트 페이지</div>;
+  const [isOn, setisOn] = useState(false);
+  const { data: beforeAuction } = useGetBeforeAuctionList();
+  const { data: afterAuction } = useGetAfterAuctionList();
+
+  return (
+    <PageWrapper>
+      <ToggleButton isOn={isOn} setisOn={setisOn} />
+      <Container>
+        {isOn
+          ? afterAuction?.data?.map((item) => {
+              if (item.end_date > getNow())
+                return <ProductCard key={item.id} {...item} />;
+            })
+          : beforeAuction?.data?.map((item) => (
+              <ProductCard key={item.id} {...item} />
+            ))}
+      </Container>
+    </PageWrapper>
+  );
 };
 
 export default List;
+
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 15px 15px;
+  max-width: 360px;
+`;
