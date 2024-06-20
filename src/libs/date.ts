838223af9f0413e65c9ffb9c0ee0d6eeb32d1de9
ppacman <smwfkim2@naver.com>
2024-06-20 13:29:16 +0900

    feat. 차트 위한 날짜 lib 구현

--- src/libs/date.ts
+++ src/libs/date.ts
@@ -22,3 +22,14 @@ export const formattingDate = (str: string) => {
 
   return formattedDate;
 };
+
+export const detailFormattingDate = (str: string) => {
+  const date = new Date(str);
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const seconds = date.getSeconds().toString().padStart(2, '0');
+
+  const formattedDate = `${month}-${day}/${minutes}:${seconds}`;
+  return formattedDate;
+};
